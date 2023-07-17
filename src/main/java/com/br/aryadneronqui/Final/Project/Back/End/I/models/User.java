de181f6abf96fc6b70ba38f5632e77676afd32d3
Aryadne Fernanda Ronqui <aryadne.fernanda@gmail.com>
2023-07-17 10:56:24 -0400

    fixed cross Origin

--- src/main/java/com/br/aryadneronqui/Final/Project/Back/End/I/models/User.java
+++ src/main/java/com/br/aryadneronqui/Final/Project/Back/End/I/models/User.java
@@ -1,15 +1,19 @@
 package com.br.aryadneronqui.Final.Project.Back.End.I.models;
 
 import com.br.aryadneronqui.Final.Project.Back.End.I.dtos.CreateUser;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 
 @Getter
+@Entity
+@Table(name="users")
 
 public class User {
-
+    @Column
     private String name;
     private String email;
     private String password;
@@ -22,6 +26,12 @@ public class User {
         password = newUser.password();
         tasks = new ArrayList<>();
     }
+    public String generateToken(){
+        tokenLogin = UUID.randomUUID().toString();
+        return tokenLogin;
+    }
 
-
+    public boolean isAuthenticated(String token){
+        return tokenLogin != null && tokenLogin.equals(token);
+    }
 }
